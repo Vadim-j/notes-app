800476fc1fff9753778818be2d37bbd949a0ec67
Vadim-j <vadimmurahovskij99@gmail.com>
2024-05-19 18:57:49 +0300

    добавили сохранение файлов

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from library import *
-add_note("заметка 3")
+save_to_file("save",["a","!","i"])
+notes = load_from_file("save")
+notes = add_note("заметка 3",notes)
 display_notes(notes)  
+save_to_file("save",notes)
--- library.py
+++ library.py
@@ -1,7 +1,19 @@
-notes = ["заметка 1","заметка 2"]
+#notes = ["заметка 1","заметка 2"]
 def display_notes(note_list):
     for note in note_list:
         print(note)
 
-def add_note(text):
+
+def add_note(text,notes):
     notes.append(text)
+    return notes
+
+def save_to_file(file_name,data_list):
+    with open(file_name+".txt","w",encoding="utf-8")as file:
+        file.write("/-".join(data_list))
+
+
+def load_from_file(file_name):
+    with open(file_name+".txt","r",encoding="utf-8") as file:
+        info = file.read().split("/-")
+    return info
