c8ab7f1d6ef474101b9a0d46221eec88536bb9fa
Vadim-j <vadimmurahovskij99@gmail.com>
2024-06-01 18:59:13 +0300

    добавили удаление и  просмотр заметок

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from library import *
-save_to_file("save",["a","!","i"])
-notes = load_from_file("save")
-notes = add_note("заметка 3",notes)
-display_notes(notes)  
+import os 
+options = ["an!","aq!","as!","ad!","ar!"]
+if os.path.isfile("save.txt"):
+    notes = load_from_file("save")
+else:
+    notes = []
+
+user_input = ""
+while user_input!="aq!":
+    user_input = input("введите:\nan!-добавить новую заметку\naq!-выход из программы\nar!-удалить заметку\n->".lower)
+    if user_input == "an!":
+        os.system("cls")
+        user_input= input("введите текст новой заметки:\n".lower)
+        notes = add_note(user_input,notes)
+    elif user_input == "ad!":
+      display_notes(notes)    
+    elif user_input == "as!":
+        save_to_file("save",notes)
+        print("заметки сохранены успешно")
+    elif user_input == "ar!":
+        user_input = int(input("введите номер заметки которую хотите удалить:".lower))
+        notes = delete_note(user_input,notes)
 save_to_file("save",notes)
--- library.py
+++ library.py
@@ -1,19 +1,39 @@
 #notes = ["заметка 1","заметка 2"]
+import os 
 def display_notes(note_list):
+    """
+    принимает : список заметок 
+    показывает список заметок на экране
+    """
+    os.system("cls")
+    print("все заметки:".upper())
     for note in note_list:
         print(note)
 
 
 def add_note(text,notes):
+    """
+    добавляет заметки 
+    """
     notes.append(text)
     return notes
 
 def save_to_file(file_name,data_list):
+    """
+    сохраняет заметки
+    """
     with open(file_name+".txt","w",encoding="utf-8")as file:
         file.write("/-".join(data_list))
 
 
 def load_from_file(file_name):
+    """
+    загружает заметки в файл
+    """
     with open(file_name+".txt","r",encoding="utf-8") as file:
         info = file.read().split("/-")
     return info
+
+def delete_note(index,notes):
+    del notes[index]
+    return notes
